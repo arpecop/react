1164c620e09adbde6ab7cc02e9a6fdf048e4388a
Rudi <rudix.lab@gmail.com>
2019-10-20 12:54:56 +0300

    c

--- src/Apps/incubator/Bottom.js
+++ src/Apps/incubator/Bottom.js
@@ -25,9 +25,34 @@ const Listx = ({ prefix, rows }) => (
   />
 );
 
+const Amazon = ({ prefix, rows }) => (
+  <List
+    itemLayout="horizontal"
+    dataSource={rows}
+    renderItem={(item) => (
+      <a href={`/${prefix}/${item.key}`}>
+        <Badge
+          count={item.value > 1 ? item.value : 0}
+          style={{
+            backgroundColor: '#95a5a6',
+          }}
+        >
+          <Tag style={{ color: '#252425', cursor: 'pointer', margin: 4 }}>
+            {`${item.title}:${item.price}`}
+
+            {JSON.stringify(item)}
+          </Tag>
+        </Badge>
+      </a>
+    )}
+  />
+);
+
+
 const Bottom = ({ tag }) => {
   const [data, setData] = useState(null);
   const [data1, setData1] = useState(null);
+  const [amazon, setAmazon] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
@@ -43,16 +68,27 @@ const Bottom = ({ tag }) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result1 = await axios(
+      const result = await axios(
         `https://arpecop.serveo.net/twitter/_design/api/_view/tags?reduce=true&group=true&limit=20&skip=20&start_key="${
           tag
         }"&update=false`,
       );
 
-      setData1(result1.data);
+      setData1(result.data);
     };
     fetchData();
   }, [tag]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://amazonka.herokuapp.com/${tag}`,
+      );
+
+      setAmazon(result.data);
+    };
+    fetchData();
+  }, [tag]);
+
   return (
     <div>
       <div style={{ width: '70%', margin: 'auto' }}>
@@ -63,6 +99,16 @@ const Bottom = ({ tag }) => {
         <Title>Tags</Title>
 
         {data1 ? <Listx rows={data1.rows} prefix="t" /> : <Spin />}
+      </div>
+      <div style={{ width: '70%', margin: 'auto' }}>
+
+
+        {amazon[0] ? (
+          <div>
+            <Title>Products</Title>
+            <Amazon rows={amazon} prefix="t" />
+          </div>
+        ) : null}
       </div>
     </div>
   );
