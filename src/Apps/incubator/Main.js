69c8a642658789867b911229e82a236242246841
Rudi <rudix.lab@gmail.com>
2019-10-21 12:03:35 +0300

    c

--- src/Apps/incubator/Main.js
+++ src/Apps/incubator/Main.js
@@ -1,30 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Item from './components/Item';
+import React from 'react';
 
-const uuid = require('uuid/v4');
-
-const Tag = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        'https://arpecop.serveo.net/twitter/_design/api/_view/tags?reduce=false&include_docs=true&limit=22&update=false&descending=false',
-      );
-      setData(result.data);
-    };
-    fetchData();
-  }, []);
-  return (
-    <div>
-      <ul>
-        {data ? data.rows.map((item) => (
-          <Item key={uuid()} item={item.doc} />
-        )) : (<div>Loading</div>)}
-      </ul>
-    </div>
-  );
-};
+const data = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const Tag = () => (
+  <div>
+    {data.map((item) => (
+      <li key={item}>
+        <a href={`/t/${item}`}>
+          {item}
+        </a>
+      </li>
+    ))}
+  </div>
+);
 
 export default Tag;
