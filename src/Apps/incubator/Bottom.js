26e8162c7132e53d98f6c599bc9cb034431e3f63
Rudi <rudix.lab@gmail.com>
2019-10-21 16:13:41 +0300

    c

--- src/Apps/incubator/Bottom.js
+++ src/Apps/incubator/Bottom.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import {
-  List, Tag, Badge, Spin,
+  List, Tag, Badge, Spin, Row, Col,
 } from 'antd';
 import { Title } from './components/UI';
 
@@ -25,34 +25,11 @@ const Listx = ({ prefix, rows }) => (
   />
 );
 
-const Amazon = ({ prefix, rows }) => (
-  <List
-    itemLayout="horizontal"
-    dataSource={rows}
-    renderItem={(item) => (
-      <a href={`/${prefix}/${item.key}`}>
-        <Badge
-          count={item.value > 1 ? item.value : 0}
-          style={{
-            backgroundColor: '#95a5a6',
-          }}
-        >
-          <Tag style={{ color: '#252425', cursor: 'pointer', margin: 4 }}>
-            {`${item.title}:${item.price}`}
-
-            {JSON.stringify(item)}
-          </Tag>
-        </Badge>
-      </a>
-    )}
-  />
-);
-
 
 const Bottom = ({ tag }) => {
   const [data, setData] = useState(null);
   const [data1, setData1] = useState(null);
-  const [amazon, setAmazon] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
@@ -78,39 +55,26 @@ const Bottom = ({ tag }) => {
     };
     fetchData();
   }, [tag]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios(
-        `https://amazonka.herokuapp.com/${tag}`,
-      );
 
-      setAmazon(result.data);
-    };
-    fetchData();
-  }, [tag]);
 
   return (
     <div>
-      <div style={{ width: '70%', margin: 'auto' }}>
-        <Title>Users</Title>
-        {data ? <Listx rows={data.rows} prefix="u" /> : <Spin />}
-      </div>
-      <div style={{ width: '70%', margin: 'auto' }}>
-        <Title>Tags</Title>
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={20} md={18} lg={10}>
 
-        {data1 ? <Listx rows={data1.rows} prefix="t" /> : <Spin />}
-      </div>
-      <div style={{ width: '70%', margin: 'auto' }}>
+          <Title>Users</Title>
+          {data ? <Listx rows={data.rows} prefix="u" /> : <Spin />}
 
 
-        {amazon[0] ? (
-          <div>
-            <Title>Products</Title>
-            <Amazon rows={amazon} prefix="t" />
-          </div>
-        ) : null}
-      </div>
+          <Title>Tags</Title>
+
+          {data1 ? <Listx rows={data1.rows} prefix="t" /> : <Spin />}
+
+
+        </Col>
+      </Row>
     </div>
+
   );
 };
 
