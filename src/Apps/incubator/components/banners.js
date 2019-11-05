8a3cc03bd5aed959f311b4c95cf70aac49dc0765
Rudi <rudix.lab@gmail.com>
2019-11-05 18:46:50 +0200

    c

--- src/Apps/incubator/components/banners.js
+++ src/Apps/incubator/components/banners.js
@@ -14,6 +14,22 @@ const banners = [
     url: 'https://www.amazon.com/dp/B002I0J4VQ?tag=rudix-20&linkCode=ur1',
     img: 'https://images-na.ssl-images-amazon.com/images/G/01/img09/video-games/associates/leaderboard/amazon-ps3-120_728x90.png',
   },
+  {
+    url: 'https://www.zazzle.co.uk/tanktops?rf=238112296902500890&CMPN=ban_custom_tank_tops_2015',
+    img: 'https://asset.zcache.co.uk/assets/graphics/z4/uniquePages/banners/Tank_Tops_2015_728x90.jpg',
+  },
+  {
+    url: 'https://www.amazon.com/b?tag=rudix-20&linkCode=ur1&node=5174',
+    img: 'https://images-na.ssl-images-amazon.com/images/G/01/img09/associates/med-rec/music_med-rec.gif',
+  },
+  {
+    url: 'https://www.amazon.com/music/unlimited/hd?tag=rudix-20&linkCode=ur1',
+    img: 'https://images-na.ssl-images-amazon.com/images/G/01/associates/080119_US_Katana_ACQ_PD_eg_CV8E_468x60._CB1569025332_.jpg',
+  },
+  {
+    url: 'https://www.amazon.com/dp/B074TJCK8Y/ref=twister_B06XDGTPDP?tag=rudix-20&linkCode=ur1&_encoding=UTF8&psc=1',
+    img: 'https://www.amazon.com/dp/B074TJCK8Y/ref=twister_B06XDGTPDP?tag=rudix-20&linkCode=ur1&_encoding=UTF8&psc=1',
+  },
 ];
 const WrapperBanner = () => {
   const item = shuffle(banners)[0];
