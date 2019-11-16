6b4c6f8a5cfdd82cd992a92310b69e45243ba7e0
Rudi <rudix.lab@gmail.com>
2019-11-16 18:00:07 +0200

    c

--- src/Apps/incubator/components/TextFormat.js
+++ src/Apps/incubator/components/TextFormat.js
@@ -6,45 +6,47 @@ import {
 const uuid = require('uuid/v4');
 
 const TextFormat = ({ text }) => {
-  const arr = text.split(' ').map((word) => {
-    if (word.substring(0, 1) === '#') {
-      return { type: 'tag', word, key: uuid() };
-    }
-    if (word.substring(0, 3) === 'http') {
-      return { type: 'url', word: 'xxx', key: uuid() };
-    }
-    return { word, type: 'word', key: uuid() };
-  });
-  const cont = arr.map((word) => {
-    if (word.type === 'url') {
-      return <React.Fragment key={word.key}>dsdsxxxxx</React.Fragment>;
-    }
-    if (word.type === 'tag') {
-      return (
-        <a
-          key={word.key}
-          href={`/t/${word.word
-            .toLowerCase()
-            .replace('#', '')
-            .replace('.', '')}`}
-        >
-          <Tag style={{
-
-            border: 'none',
-
-            cursor: 'pointer',
-            margin: 4,
-          }}
+  if (text) {
+    const arr = text.split(' ').map((word) => {
+      if (word.substring(0, 1) === '#') {
+        return { type: 'tag', word, key: uuid() };
+      }
+      if (word.substring(0, 3) === 'http') {
+        return { type: 'url', word: 'xxx', key: uuid() };
+      }
+      return { word, type: 'word', key: uuid() };
+    });
+    const cont = arr.map((word) => {
+      if (word.type === 'url') {
+        return <React.Fragment key={word.key}>dsdsxxxxx</React.Fragment>;
+      }
+      if (word.type === 'tag') {
+        return (
+          <a
+            key={word.key}
+            href={`/t/${word.word
+              .toLowerCase()
+              .replace('#', '')
+              .replace('.', '')}`}
           >
-            {word.word.replace('#', '')}
+            <Tag style={{
 
-          </Tag>
-        </a>
-      );
-    }
-    return <React.Fragment key={word.key}>{`${word.word} `}</React.Fragment>;
-  });
+              border: 'none',
 
-  return cont;
+              cursor: 'pointer',
+              margin: 4,
+            }}
+            >
+              {word.word.replace('#', '')}
+
+            </Tag>
+          </a>
+        );
+      }
+      return <React.Fragment key={word.key}>{`${word.word} `}</React.Fragment>;
+    });
+    return cont;
+  }
+  return '';
 };
 export default TextFormat;
