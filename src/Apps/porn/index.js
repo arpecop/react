00f6e4f5d3c74cb83f352c7504697617a24698d2
Radoslav Georgiev <rudix.lab@gmail.com>
2020-03-16 12:51:08 +0200

    c

--- src/Apps/porn/index.js
+++ src/Apps/porn/index.js
@@ -6,7 +6,7 @@ import 'antd/dist/antd.css';
 // or 'antd/dist/antd.less'
 function App(props) {
   const { isIndex, match } = props;
-  const [{ data, loading, error }] = useAxios(
+  const [{ data, loading }] = useAxios(
     `https://db.arpecop.xyz/porn/_all_docs?limit=25&include_docs=true&${isIndex ? '' : `&start_key="${match.params.id}"`}`,
   );
 
@@ -15,20 +15,7 @@ function App(props) {
     <div>
       <Helmet>
         <title>{data.rows[0].doc.t}</title>
-        <meta
-          property="og:url"
-          content={`https://kartinki.netlify.com/${data.rows[0].key}`}
-        />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="kartinki" />
-
-        <meta
-          property="og:image"
-          content={`https://s3.eu-west-1.amazonaws.com/imgserve.fbook.space/${data.rows[0].value.md5}.jpg`}
-        />
-        <meta property="og:image:width" content={data.rows[0].value.w} />
-        <meta property="og:image:height" content={data.rows[0].value.h} />
-        <meta property="fb:app_id" content="770341770061627" />
+        <meta name="google-site-verification" content="PpcXwvxVoie3FL806wiJD3i3U-lBT2cA0mjU3OWCqW0" />
       </Helmet>
       <h1>{data.rows[0].doc.t}</h1>
       <div style={{ textAlign: 'center' }}>
