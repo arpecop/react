64a5e53787fb6c27b75c807eb7c18cee5d719943
Radoslav <rudix.lab@gmail.com>
2020-09-19 11:25:39 +0300

    c

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 const app = process.env.appname;
 const dir = __dirname.replace('scripts', '');
 const assets = require(`${dir}/build/asset-manifest.json`);
-console.log('==========', assets, '========');
+console.log('==========', assets, '==========');
